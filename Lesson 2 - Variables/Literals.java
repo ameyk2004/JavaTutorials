94a176ce3e473bd4a637d0671ca7d8b04bc719b8
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-11 17:47:35 +0530

    Literals in Java

---
+++ Lesson 2 - Variables/Literals.java
@@ -0,0 +1,11 @@
+public class Literals {
+    public static void main(String args[])
+    {
+        // literals
+
+        double num1 = 1000000; // counting zeros can be diffuclut
+        double num = 100_000_00; // seperate with underscores
+        
+    }
+
+}
