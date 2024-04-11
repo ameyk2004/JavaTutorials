28a5a9d0dc3981ee32c01079f154bf653222fcd5
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-11 17:16:16 +0530

    Lesson 2 - Variables

--- Lesson 1 - Set Up/Hello.java
+++ Lesson 1 - Set Up/Hello.java
@@ -1,9 +1,18 @@
 public class Hello {
     
-    //My first Java Code to print Hello World
-
+    // This is a Java class named Hello.
+    
+    // My first Java Code to print Hello World
+    
+    // The main method is the entry point of the program.
+    // It is a special method that Java looks for to start executing the program.
+    // It is declared as public, static, and void, meaning it can be accessed by any class,
+    // doesn't belong to any specific object, and doesn't return any value.
+    // It takes an array of strings (args) as input parameters.
+    
     public static void main(String args [])
     {
+        // The System.out.println() statement prints the string "!!! Hello World !!!" to the console.
         System.out.println("!!! Hello World !!!");
     }
 }
---
+++ Lesson 2 - Variables/Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    // Variables allows us to store data 
+
+    // Variables are used to store data in a program. They have a data type, a name, and a value.
+    // In Java, variables must be declared with a specific data type before they can be used.
+
+    // Variable declaration involves specifying the data type and the variable name.
+    // Syntax: data_type variable_name;
+
+    public static void main(String args[])
+    {
+        int num;
+        num = 10;
+
+        System.out.println(num);
+    }
+   
+
+
+}  
