d6a3034d56c7b25ef214398e217a6b00b12c69e0
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-13 17:27:40 +0530

    while loop

---
+++ Lesson 6 - Loops/While Loop/WhileLoop.java
@@ -0,0 +1,15 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        // Initialization
+        int i = 0;
+
+        // While loop
+        while (i < 5) {
+            // Code to be executed in each iteration
+            System.out.println("i: " + i);
+            
+            // Increment
+            i++;
+        }
+    }
+}
