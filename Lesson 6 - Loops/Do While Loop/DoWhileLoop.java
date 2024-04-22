5ec7cf8315aba39bccd7669f0fbcfb4e44eed4a6
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-22 17:46:05 +0530

    added do While loop tutorial

---
+++ Lesson 6 - Loops/Do While Loop/DoWhileLoop.java
@@ -0,0 +1,15 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        // Initialization
+        int i = 0;
+
+        // Do-while loop
+        do {
+            // Code to be executed in each iteration
+            System.out.println("i: " + i);
+            
+            // Increment
+            i++;
+        } while (i < 5); // Condition check comes after the loop body
+    }
+}
