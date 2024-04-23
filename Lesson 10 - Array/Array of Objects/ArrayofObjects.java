c59daac736eec80128f18981e69e4967ab20cb6a
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-23 10:54:53 +0530

    normal ways of traversing

---
+++ Lesson 10 - Array/Array of Objects/ArrayofObjects.java
@@ -0,0 +1,18 @@
+class Student{
+    int rollno;
+    int marks;
+    String name;
+}
+
+public class ArrayofObjects {
+    public static void main(String[] args) {
+        //Normal ways of creating traversing
+        // int nums[] = new int[4];
+
+        // for(int n : nums)
+        // {
+        //     System.out.print(n + " ");
+        // }
+    }
+    
+}
