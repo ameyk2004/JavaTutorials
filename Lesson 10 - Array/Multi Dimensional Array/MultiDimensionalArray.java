566ef1a25bc1c442e8c35585cab7d62c40000eb9
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-23 10:39:07 +0530

    enhached For loop

--- Lesson 10 - Array/Multi Dimensional Array/MultiDimensionalArray.java
+++ Lesson 10 - Array/Multi Dimensional Array/MultiDimensionalArray.java
@@ -18,10 +18,27 @@ public class MultiDimensionalArray{
             for(int j = 0;j<multiDimArr[0].length; j++)
             {
                 int num = (int)(Math.random()*10);
+                multiDimArr[i][j] = num;
                 System.out.print(num+ " ");
             }
             System.out.println();
         }
+
+
+        // Enhanced For Loop For Traversing
+
+        System.out.println("\nEnhanced For loop Traversing");
+
+        for(int n[] : multiDimArr)
+        {
+            for(int i : n)
+            {
+                System.out.print(i+ " ");
+            }
+            System.out.println();
+        }
+
+
 
     }
 };
