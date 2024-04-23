254dec2aee4a2d8bdab3407b4bd0f7075f803d74
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-23 10:48:19 +0530

    jagged array syntax

--- Lesson 10 - Array/Jagged Arrays/JaggedArrays.java
+++ Lesson 10 - Array/Jagged Arrays/JaggedArrays.java
@@ -1,7 +1,22 @@
 public class JaggedArrays {
     public static void main(String[] args) {
-        
-    }
-}
+        // Declare and initialize a jagged array
+        int nums[][] = new int[4][]; // Jagged Array
+
+        // Define the size of each sub-array
+        nums[0] = new int[2];
+        nums[1] = new int[3];
+        nums[2] = new int[4];
+        nums[3] = new int[2];
+
     
 
+        // Print the elements of the jagged array
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
