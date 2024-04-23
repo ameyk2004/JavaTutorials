a343c6b08db8b142c84ba0d49ae91f08fb107663
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-23 11:16:07 +0530

    string methods

--- Lesson 11 - Strings/Strings.java
+++ Lesson 11 - Strings/Strings.java
@@ -3,5 +3,15 @@ public class Strings {
     {
         String name = new String("My Name");
         System.out.println(name);
+        System.out.println(name.hashCode());
+
+        System.out.println("Length of String: " + name.length());
+        System.out.println("Character at index 3: " + name.charAt(3));
+        System.out.println("Substring from index 3: " + name.substring(3));
+        System.out.println("Substring from index 3 to 6: " + name.substring(3, 7));
+        System.out.println("Index of 'N': " + name.indexOf('N'));
+        System.out.println("Index of 'x': " + name.indexOf('x'));
+        System.out.println("String starts with 'My': " + name.startsWith("My"));
+        System.out.println("String ends with 'me': " + name.endsWith("me"));
     }
 }
