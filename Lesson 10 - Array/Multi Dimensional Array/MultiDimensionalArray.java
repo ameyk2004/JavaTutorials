ae20ec6b77f0e7f3b4e56a371cd908a92ef8e6a0
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-23 10:28:23 +0530

    Traversing MD Array

--- Lesson 10 - Array/Multi Dimensional Array/MultiDimensionalArray.java
+++ Lesson 10 - Array/Multi Dimensional Array/MultiDimensionalArray.java
@@ -1,9 +1,20 @@
 public class MultiDimensionalArray{
     public static void main(String args[])
     {
-        int multiDimArr[][] = new int[6][5];
+        int multiDimArr[][] = new int[4][4];
 
-        System.out.println(multiDimArr[0][0]);
+        // System.out.println(multiDimArr[0][0]);
+
+
+        //Traversing
+        for(int i=0;i<multiDimArr.length ;i++)
+        {
+            for(int j = 0;j<multiDimArr[0].length; j++)
+            {
+                System.out.print(multiDimArr[i][j]+ " ");
+            }
+            System.out.println();
+        }
 
     }
 };
