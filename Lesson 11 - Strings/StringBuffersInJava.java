ef629fd2c0875ea5f23429605819e8cfd52001db
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-23 16:39:03 +0530

    Intialize String Buffer

---
+++ Lesson 11 - Strings/StringBuffersInJava.java
@@ -0,0 +1,8 @@
+public class StringBuffersInJava {
+    public static void main(String[] args) {
+        System.out.println("string buffers");
+
+        StringBuffer sb = new StringBuffer("Amey");
+        System.out.println(sb.capacity());
+    }
+}
--- Lesson 11 - Strings/StringConstantPool.java
+++
@@ -1,5 +0,0 @@
-public class StringConstantPool {
-    public static void main(String[] args) {
-        System.out.println("String Buffer");
-    }
-}
