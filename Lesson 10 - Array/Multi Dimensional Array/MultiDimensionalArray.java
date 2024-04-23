31840f18f838970aee7ee9f7081b022113fc63dd
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-23 10:26:01 +0530

    added syntax for multidimensional array

---
+++ Lesson 10 - Array/Multi Dimensional Array/MultiDimensionalArray.java
@@ -0,0 +1,9 @@
+public class MultiDimensionalArray{
+    public static void main(String args[])
+    {
+        int multiDimArr[][] = new int[6][5];
+
+        System.out.println(multiDimArr[0][0]);
+
+    }
+};
