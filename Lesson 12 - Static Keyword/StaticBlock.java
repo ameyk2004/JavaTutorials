b7b83ec8554c9df7154742522bb2f7aacaf1bd74
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-23 17:08:40 +0530

    static block call by Class loader

--- Lesson 12 - Static Keyword/StaticBlock.java
+++ Lesson 12 - Static Keyword/StaticBlock.java
@@ -16,13 +16,21 @@ class Sample{
 
 
 public class StaticBlock {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         System.out.println("Static Block Example");
 
-        Sample s1 = new Sample(0);
+        // Sample s1 = new Sample(0);
 
         //As you notice, Instantiating object will cause a call of Static Block
 
+
+        //How to Load a class Without creating instance ??
+        //Here the java class "Class" come to help
+
+        Class.forName("Sample");
+
+        //As you can see we have loaded the static block without instantiating the object
+        
 
 
     }
