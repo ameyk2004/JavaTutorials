945556114149aa469a39921b94fc234518eea83b
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-23 10:01:03 +0530

    modified calculator

--- Lesson 9 - Method Overloading/MethodOverloading.java
+++ Lesson 9 - Method Overloading/MethodOverloading.java
@@ -1,22 +1,24 @@
-class Calculator{
-
-    int add(int a, int b)
-    {
-        return a+b;
-    }
-   
-    int add(int a, int b, int c)
-    {
-        return a + b +c;
+class Calculator {
+    // First add method that takes two integers and returns their sum
+    int add(int a, int b) {
+        return a + b;
     }
 
+    // Second add method that takes three integers and returns their sum
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
 
-};
-class MethodOverloading{
+public class MethodOverloading {
     public static void main(String[] args) {
+        // Creating an instance of the Calculator class
         Calculator calc = new Calculator();
 
-        System.out.println(" Addition of 2 numbers : "+ calc.add(20, 30));
-        System.out.println(" Addition of 3 numbers : "+ calc.add(20, 30, 50));
+        // Calling the first add method with two parameters and printing the result
+        System.out.println("Addition of 2 numbers: " + calc.add(20, 30));
+
+        // Calling the second add method with three parameters and printing the result
+        System.out.println("Addition of 3 numbers: " + calc.add(20, 30, 50));
     }
 }
