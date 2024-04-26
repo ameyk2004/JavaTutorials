8482b2797ffa80829eb30328ade18009857623b5
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-26 09:18:08 +0530

    initialising encapsulation

---
+++ Lesson 13 - Encapsulation/Encapsulation.java
@@ -0,0 +1,16 @@
+class Human{
+
+    //You know lot of stuff
+
+    //En capsulating , i.e hiding of data
+
+}
+
+
+public class Encapsulation {
+
+    public static void main(String args[])
+    {
+
+    }
+}
