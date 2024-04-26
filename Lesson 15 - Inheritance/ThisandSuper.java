2326d46c57072876d13313b7c3f1d8ea016993ff
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-26 10:11:16 +0530

    use of this keyword

--- Lesson 15 - Inheritance/ThisandSuper.java
+++ Lesson 15 - Inheritance/ThisandSuper.java
@@ -19,13 +19,21 @@ class B extends A
     {
         //super(); // calls default constructor of A
         super("Parameter"); // calls parameterized const of A
+
+        
         System.out.println("B constructor called");
+    }
+
+    B(String param)
+    {
+        this();
+        System.out.println("B constructor called with "+param);
     }
 }
 public class ThisandSuper {
     public static void main(String args[])
     {
-        B obj = new B();
+        B obj = new B("hello");
         System.out.println((obj.getClass())); // prints the class
 
         //first the basd class onstructor is called
