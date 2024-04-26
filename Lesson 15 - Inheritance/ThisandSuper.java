4495f19997d47b85de2a8162671b00582d32ef89
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-04-26 10:09:31 +0530

    super function usecase

--- Lesson 15 - Inheritance/ThisandSuper.java
+++ Lesson 15 - Inheritance/ThisandSuper.java
@@ -5,12 +5,20 @@ class A
     {
         System.out.println("A constructor called");
     }
+
+    A(String param)
+    {
+        System.out.println("A constructor called with "+param);
+    }
+    
 }
 
 class B extends A
 {
     B()
     {
+        //super(); // calls default constructor of A
+        super("Parameter"); // calls parameterized const of A
         System.out.println("B constructor called");
     }
 }
@@ -19,6 +27,10 @@ public class ThisandSuper {
     {
         B obj = new B();
         System.out.println((obj.getClass())); // prints the class
+
+        //first the basd class onstructor is called
+
+        
     }
     
 }
